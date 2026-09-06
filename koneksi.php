9b4dea047efd224b956e5e82b48fdb5784029313
itskristyy <adinda.kristyy1903@gmail.com>
2026-09-06 08:44:49 +0700

    feat: add database connection logic

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,4 @@
 <?php
-
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "db_skillvora";
-
-// buat koneksi ke db
-$koneksi = mysqli_connect($server, $username, $password, $database);
-
-if (!$koneksi) {
-    die("Koneksi Gagal: " . mysqli_connect_error());
-}
+$koneksi = mysqli_connect("localhost", "root", "", "db_skillvora")
+    or die("Koneksi Gagal: " . mysqli_connect_error());
+?>
